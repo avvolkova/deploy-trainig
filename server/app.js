a8078d3cb31bc4811f6c741b53b05b9c7b890feb
Anna Volkova <volkovaaaa@MacBook-Pro.local>
2024-01-25 23:51:03 +0300

    fixed fe and be connection

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 
 const app = express();
 
 app.use(express.static('/dist'));
+app.use(cors())
 
-app.get('/', (_, res) => {
+app.get('/anya', (_, res) => {
     console.log('Anya molodec!')
-    res.send('Anya molodec!')
+    res.json({whoIsAnya: 'Anya molodec!'})
 })
 
 app.get("*", (req, res) => res.redirect("/"));
 
-app.listen(process.env.PORT || 5173)
+app.listen(process.env.PORT || 5000)
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,6 +7,7 @@ export function Navbar() {
 
     useEffect(() => {
         console.log({params})
+        fetch('http://localhost:5000/anya').then(res => res.json()).then(console.log)
     }, [])
 
     return (
