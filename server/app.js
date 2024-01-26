25ceb1fc2ced3bd148232e3905cb37c45e1388af
Anna Volkova <volkovaaaa@MacBook-Pro.local>
2024-01-26 11:58:20 +0300

    last hope

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import * as path from "path";
 
 
 const app = express();
@@ -7,10 +8,19 @@ const app = express();
 app.use(express.static('/dist'));
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.get('/anya', (_, res) => {
     console.log('Anya molodec!')
     res.json({whoIsAnya: 'Anya molodec х3!'})
 })
+
+app.get("*", (req, res) => res.redirect("/"));
+
 
 app.listen(process.env.PORT || 5000)
